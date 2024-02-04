59c50c2428c0976d30792cefb26a7897ab673553
Thymo van Beers <thymovanbeers@gmail.com>
2024-02-04 10:43:23 +0100

    Support Gentoo for LLVM17

--- llvm_config_linux_llvm17.go
+++ llvm_config_linux_llvm17.go
@@ -2,9 +2,9 @@
 
 package llvm
 
-// #cgo CPPFLAGS: -I/usr/include/llvm-17 -I/usr/include/llvm-c-17 -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
+// #cgo CPPFLAGS: -I/usr/include/llvm-17 -I/usr/include/llvm-c-17 -I/usr/lib/llvm/17/include -I/usr/lib/llvm/17/include/llvm-c -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS
 // #cgo CXXFLAGS: -std=c++17
-// #cgo LDFLAGS: -L/usr/lib/llvm-17/lib  -lLLVM-17
+// #cgo LDFLAGS: -L/usr/lib/llvm-17/lib -L/usr/lib/llvm/17/lib64  -lLLVM-17
 import "C"
 
 type run_build_sh int
